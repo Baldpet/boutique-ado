e16bc3857d0ab36870deba0d0e2e9da1c74ac447
Baldpet <peterdaniel.baldwin@gmail.com>
2020-12-06 12:43:14 +0000

    webhooks implemented and linked to the handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -13,6 +13,26 @@ class StripeWH_Handler:
         """
 
         return HttpResponse(
+            content=f'Unhandled webhook recieved: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent succeeded webhook event
+        """
+
+        return HttpResponse(
+            content=f'Webhook recieved: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent failed webhook event
+        """
+
+        return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
             status=200
         )
