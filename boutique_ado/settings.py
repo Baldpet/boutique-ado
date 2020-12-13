3f7dbdf7de15c6ea6475031389d8519474bba15d
Baldpet <peterdaniel.baldwin@gmail.com>
2020-12-13 15:19:30 +0000

    removed ecret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's$!cyyw8q967j-s*0ygwi7ici!^=l*v#ky(qcle68+957)x1g)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['baldpet-boutique-ado.herokuapp.com', 'localhost']
 
